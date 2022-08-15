ac17c9c1a9c725f27c7e4719f355952ab31bd3ab
Fahdi Labib <fahdilabib@gmail.com>
2022-08-16 03:53:17 +0700

    Fixing laravel custom configuration

--- src/Config.php
+++ src/Config.php
@@ -4,12 +4,12 @@ namespace Flabib\XRay;
 
 class Config
 {
-    const config = [
-        'port' => 1945,
-        'host' => 'localhost'
-    ];
-
     public static function get(string $key) {
-        return self::config[$key];
+        switch ($key) {
+            case 'port':
+                return config('xray.port') ?: 1945;
+            case 'host':
+                return config('xray.host') ?: 'localhost';
+        }
     }
 }
